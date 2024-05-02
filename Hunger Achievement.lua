018237525c7a56e1b631a3d880e5434ce0959612
opposition pantophobia <162717999+Drop56796@users.noreply.github.com>
2024-05-02 12:40:46 +0800

    Create Hunger Achievement.lua

---
+++ Hunger Achievement.lua
@@ -0,0 +1,10 @@
+---====== Load achievement giver ======---
+local achievementGiver = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+
+---====== Display achievement ======---
+achievementGiver({
+    Title = "Give me you soul",
+    Desc = "I'm Hunger of you",
+    Reason = "Survive the Hunger",
+    Image = "rbxassetid://17356445846"
+})
